d132e409e24391464a5d3eb0b01d48aff8a85ffc
Nikolay Buldakov <bul-nick-al@2ya.ru>
2021-05-25 21:34:55 +0300

    Add the main App

---
+++ KiwiSuggestions/KiwiSuggestions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KiwiSuggestions
+//
+//  Created by Николай Булдаков on 25.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
